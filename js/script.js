48973acf13e8118861659f1f87efafab5c03a459
Kleber Henrique <yuk9.gabriel@gmail.com>
2023-08-01 23:46:15 -0300

    update

--- js/script.js
+++ js/script.js
@@ -16,6 +16,9 @@ form.addEventListener("submit", (event) => {
 
   const height = document.getElementById("height").value;
   const weight = document.getElementById("weight").value;
+  const name = document.getElementById("name").value.split(' ')[0];
+
+  console.log(name)
 
   let imc = weight / (height * height);
 
@@ -26,19 +29,19 @@ form.addEventListener("submit", (event) => {
   document.getElementById("infos").classList.remove("hidden");
 
   if (imc <= 18.5) {
-    description = "Abaixo do peso";
+    description = `${name} você está abaixo do peso`;
   } else if (imc >= 18.5 && imc <= 25) {
     value.classList.remove("attention");
     value.classList.add("normal");
-    description = "Peso ideal";
+    description = `${name} você está no seu Peso ideal`;
   } else if (imc >= 25 && imc <= 30) {
-    description = "Levemente acima do peso";
+    description = `${name} você está levemente acima do peso`;
   } else if (imc >= 30 && imc <= 35) {
-    description = "Obesidade grau I";
+    description = `${name} você está com obesidade grau I`;
   } else if (imc >= 35 && imc <= 40) {
-    description = "Obesidade severa";
+    description = `${name} você está com obesidade severa`;
   } else {
-    description = "Obesidade mórbida";
+    description = `${name} você está obesidade mórbida`;
   }
 
   value.textContent = imc.toFixed(2).replace(".", ",");
